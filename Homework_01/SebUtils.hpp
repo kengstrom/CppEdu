04e00bd3d70c293934a47b5f3385e6563a7837ba
Karl Engström <karl.engstrom@seb.se>
2015-10-21 20:21:01 +0200

    Initial commit of sebutils header file.

---
+++ Homework_01/SebUtils.hpp
@@ -0,0 +1,25 @@
+/*
+ * SebUtils.hpp
+ *
+ *  Created on: Oct 21, 2015
+ *      Author: karl
+ */
+
+#ifndef SEBUTILS_HPP_
+#define SEBUTILS_HPP_
+
+namespace seb {
+	namespace str {
+
+	}
+	namespace io {
+
+	}
+	namespace gen {
+
+	}
+}
+
+
+
+#endif /* SEBUTILS_HPP_ */
