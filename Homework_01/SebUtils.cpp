79a6b27c231523cedb2e7be6a49d3b612630f812
Karl Engström <karl.engstrom@seb.se>
2015-10-21 21:04:50 +0200

    adding some functions to sebutils

---
+++ Homework_01/SebUtils.cpp
@@ -0,0 +1,22 @@
+#include "SebUtils.hpp"
+
+namespace seb {
+	namespace str {
+		string lowercase(const string& inStr) {
+			string retStr(inStr);
+			std::transform(retStr.begin(), retStr.end(), retStr.begin(), ::tolower );
+			return retStr;
+		}
+		string uppercase(const string& inStr) {
+			string retStr(inStr);
+			std::transform(retStr.begin(), retStr.end(), retStr.begin(), ::toupper );
+			return retStr;
+		}
+	}
+
+	namespace io {
+	}
+
+	namespace gen {
+	}
+}
--- Homework_01/SebUtils.hpp
+++ Homework_01/SebUtils.hpp
@@ -8,15 +8,22 @@
 #ifndef SEBUTILS_HPP_
 #define SEBUTILS_HPP_
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
 namespace seb {
 	namespace str {
-
+		string lowercase(const string&);
+		string uppercase(const string&);
 	}
+
 	namespace io {
-
 	}
-	namespace gen {
 
+	namespace gen {
 	}
 }
 
